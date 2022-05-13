599affdd6da69dc2946b3a929c286e14cc0e2bae
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2022-05-13 21:36:35 +0200

    removed infinete loop

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
-import { sendIp, my_id, sendPort } from "./consts";
+import { sendIp, my_id, sendPort, shareData } from "./consts";
 import { Server } from "socket.io";
 import { IpRequest, IpRequestCollection } from "./interfaces";
 import { getMyIp } from "./helpers";
@@ -10,6 +10,12 @@ export const connectionsData: IpRequestCollection = {};
 export const sockets = {} as { [key: string]: Socket };
 
 const myServer = new Server({ transports: ["websocket"] });
+myServer.on("connection", (socket) => {
+  socket.on(
+    shareData,
+    (property, value) => (nonReactiveBasket[property] = value)
+  );
+});
 
 const coordinatorClient = io("ws://127.0.0.1:3000", {
   transports: ["websocket"],
@@ -45,4 +51,15 @@ coordinatorClient.on(sendPort, (data: IpRequest | IpRequest[]) => {
   } else {
     processData(data);
   }
+});
+
+const nonReactiveBasket = {} as { [key: string]: any };
+export const reactiveBasket = new Proxy(nonReactiveBasket, {
+  set(target, property: string, value) {
+    target[property] = value;
+    Object.values(sockets).forEach((socket) =>
+      socket.emit(shareData, property, value)
+    );
+    return true;
+  },
 });
--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,6 @@ export const sendIp = "sendIp";
 export const sendPort = "sendPort";
 export const directPing = "directPing";
 export const getOthersData = "getOtherIps";
+export const shareData = "shareData";
 export const port = 3000;
 export const my_id = Number(process.env.pm_id);
